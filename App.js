b091acedc809946689771591572c6d65883bf590
ashwani099 <120761985+ashwani099@users.noreply.github.com>
2023-10-31 01:26:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+// import Counter from "./hooks_/Counter"
+// import Hideandshow from './hooks_/Hide_show.jsx'
+// import StatewithObject from './hooks_/StatewithObject.jsx'
+// import Main from './assignment/Main.jsx'
+// import ArrayState from "./hooks_/ArrayState"
+import Todolist from "./Todolist/Todolist"
+function App(){
+  return (
+    <div>
+  <Todolist/>
+    </div>
+  )
+}
+export default App
